b83658cef53131d3164b8b69bca836aba5afdafb
Andrea Thomaz <athomaz@ece.utexas.edu>
2016-07-22 14:19:40 -0500

    first commit, but something is broken still

---
+++ src/manipulation/arm_moveit.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import moveit_msgs.srv
+import geometry_msgs.msg
+import std_msgs.msg
+import wpi_jaco_msgs.msg
+import wpi_jaco_msgs.srv
+from math import pi
+
+class ArmMoveIt:
+
+  def __init__(self):
+    # self.pose = geometry_msgs.msg.PoseStamped()
+    ## Instantiate a RobotCommander object.  This object is an interface to
+    ## the robot as a whole.
+    self.robot = moveit_commander.RobotCommander()
+
+    ## Instantiate a PlanningSceneInterface object.  This object is an interface
+    ## to the world surrounding the robot.
+    self.scene = moveit_commander.PlanningSceneInterface()
+
+    ## Instantiate a MoveGroupCommander object.  This object is an interface
+    ## to one group of joints.  In this case the group is the joints in the left
+    ## arm.  This interface can be used to plan and execute motions on the left
+    ## arm.
+    self.group = [moveit_commander.MoveGroupCommander("arm"), moveit_commander.MoveGroupCommander("gripper"),
+         moveit_commander.MoveGroupCommander("head")]
+
+    self.planner = "RRTConnectkConfigDefault";
+
+  def get_IK(self, newPose, root = None):
+    ## from a defined newPose (geometry_msgs.msg.Pose()), retunr its correspondent joint angle(list)
+    rospy.wait_for_service('compute_ik')
+    compute_ik = rospy.ServiceProxy('compute_ik', moveit_msgs.srv.GetPositionIK)
+
+    wkPose = geometry_msgs.msg.PoseStamped()
+    if root is None:
+      wkPose.header.frame_id = self.group[0].get_planning_frame() # name:odom
+    else:
+      wkPose.header.frame_id = root
+
+    wkPose.header.stamp=rospy.Time.now()
+    wkPose.pose=newPose
+
+    msgs_request = moveit_msgs.msg.PositionIKRequest()
+    msgs_request.group_name = self.group[0].get_name() # name: arm
+    # msgs_request.robot_state = robot.get_current_state()
+    msgs_request.pose_stamped = wkPose
+    msgs_request.timeout.secs = 2
+    msgs_request.avoid_collisions = False
+
+    try:
+      jointAngle=compute_ik(msgs_request)
+      ans=list(jointAngle.solution.joint_state.position[1:7])
+      if jointAngle.error_code.val == -31:
+        print 'No IK solution'
+        return None
+      return ans
+
+    except rospy.ServiceException, e:
+      print "Service call failed: %s"%e
+
+  def get_FK(self, root = 'base_link'):
+
+    rospy.wait_for_service('compute_fk')
+    compute_fk = rospy.ServiceProxy('compute_fk', moveit_msgs.srv.GetPositionFK)
+
+    header = std_msgs.msg.Header()
+    header.frame_id = root
+    header.stamp = rospy.Time.now()
+    fk_link_names = ['right_ee_link']
+    robot_state = self.robot.get_current_state()    
+    try:
+      reply=compute_fk(header,fk_link_names,robot_state)
+      return reply.pose_stamped
+
+    except rospy.ServiceException, e:
+      print "Service call failed: %s"%e
+
+  def get_FK_wpi(self, joints = None):
+
+    rospy.wait_for_service('/jaco_arm/kinematics/fk')
+    compute_fk = rospy.ServiceProxy('/jaco_arm/kinematics/fk', wpi_jaco_msgs.srv.JacoFK)
+
+    if joints is None:
+      joints = [pi,pi,pi,pi,pi,pi,pi]    
+
+    try:
+      pose=compute_fk(joints)      
+      return pose
+
+    except rospy.ServiceException, e:
+      print "Service call failed: %s"%e
+
+
+  def plan_jointTargetInput(self,target_joint):
+    ## input: target joint angles (list) of the robot
+    ## output: plan from current joint angles to the target one
+    try:
+      self.group[0].set_joint_value_target(target_joint)
+      self.group[0].set_planner_id(self.planner)
+      planAns=self.group[0].plan()
+      return planAns
+    except:
+      print 'No plan found, see the moveit terminal for the error'
+      return None
+    
+  def plan_poseTargetInput(self,target_pose):
+    ## input: tart pose (geometry_msgs.msg.Pose())
+    ## output: plan from current  pose to the target one
+    try:
+      self.group[0].set_pose_target(target_pose)
+      self.group[0].set_planner_id(self.planner)
+      planAns=self.group[0].plan()
+      return planAns
+    except:
+      print 'No plan found, see the moveit terminal for the error'
+      return None
+  
+  def box_table_scene(self) :
+    
+    #Scene : add box 
+    # after loading this object/scene, need to do "add" ==> "planning scene"  
+    # in the rviz interface if one want to see the box
+    
+     rospy.sleep(2)
+     self.scene.remove_world_object("table_box")
+    
+     p = geometry_msgs.msg.PoseStamped()
+     p.header.frame_id = self.robot.get_planning_frame()
+     p.pose.position.x = 1.64
+     p.pose.position.y = 0.0
+     p.pose.position.z = 0.25
+     p.pose.orientation.w = 0.0
+     self.scene.add_box("table_box",p,(0.75, 1, 0.5))
+    
+     rospy.sleep(5)
+
+  def wayPointIK(self, wps, numSteps = None, ik_root = None):
+    if numSteps is None:
+      numSteps = 3
+    jointWps = []
+
+    for i in range(0, len(wps)):
+      jointP = self.get_IK(wps[i], ik_root)
+      if jointP is None:
+        jointWps = None
+        break
+      jointWps.append(jointP)
+
+    return jointWps
+
+def ask_scene_integration(arm):
+  # Ask the user if want to integrate a box scene
+  answer= input("""\n Integrate a box as a table from code ? (1 or 0)  
+  (if 1: box can't be displaced nor resized by user, if 0: no scene (can always do add from rviz interface) ) \n""")
+  
+  if answer == 1:
+    arm.box_table_scene()
+    print "\n Box inserted; to see it ==> rviz interface ==> add button==> planning scene  "
+    return
+  else:
+    print "\n No scene added"
+    return  
+  
+def ask_position(arm,tarPose):
+  #Ask the user the values of the target position
+   while True:
+    try:   
+      inputPosition=input(""" \n Target position coord. (format: x,y,z or write -1 to take the robot current position ): """)      
+    
+      if inputPosition == -1:
+        inputPosition=tarPose.position  
+        return inputPosition
+      
+    except (ValueError,IOError,NameError):
+      print("\n Please, enter the coordinate in the following format: x,y,z ")
+      continue
+    else:          
+      if len(list(inputPosition)) == 3:
+        poseTmp= geometry_msgs.msg.Pose()
+        poseTmp.position.x=inputPosition[0]
+        poseTmp.position.y=inputPosition[1]
+        poseTmp.position.z=inputPosition[2]
+        return poseTmp.position
+      else:
+        print("\n Please, enter the coordinate in the following format: x,y,z ")
+        continue
+      
+        
+def ask_orientation(arm,tarPose):
+  # Ask the user the values of the target quaternion
+  while True:
+    try:   
+      inputQuat=input(""" \n Target quaternion coordi. (format: qx,qy,qz,qw or write -1 to take the robot current quaternion ):""")
+      
+      if inputQuat == -1:
+        inputQuat=arm.group[0].get_current_pose().pose.orientation                   
+        return  inputQuat
+        
+    except (ValueError,IOError,NameError):
+      print("\n Please, enter the coordinate in the following format: qx,qy,qz,qw ")
+      continue
+    else:
+      if len(list(inputQuat)) == 4:
+        poseTmp= geometry_msgs.msg.Pose()
+        poseTmp.orientation.x=inputQuat[0]
+        poseTmp.orientation.y=inputQuat[1]
+        poseTmp.orientation.z=inputQuat[2]
+        poseTmp.orientation.w=inputQuat[3]
+        return poseTmp.orientation    
+      else:
+        print("\n Please, enter the coordinate in the following format: qx,qy,qz,qw ")
+        
+def main():
+  arm = ArmMoveIt()
+
+  tarPose = geometry_msgs.msg.Pose()
+
+  ## ask if integrate object scene from code or not
+  ask_scene_integration(arm)
+  
+  while not rospy.is_shutdown():
+    
+    ##   Assigned tarPose the current Pose of the robot 
+    tarPose = arm.group[0].get_current_pose().pose
+  
+
+    ## ask input from user (COMMENT IF NOT USE AND WANT TO ASSIGN MANUAL VALUE IN CODE)    
+    tarPose.position = ask_position(arm,tarPose)   
+    tarPose.orientation = ask_orientation(arm,tarPose)  
+
+   ##  Example of Assigned values for new targetPose to robot
+#    tarPose.position.x = 0.89
+#    tarPose.position.y = 0.00
+#    tarPose.position.z = 0.32   
+#    tarPose.orientation.x = 0.0     
+                  
+    print '\n The target coordinate is: %s \n' %tarPose     
+    
+    ## IK for target position  
+    jointTarg = arm.get_IK(tarPose)
+    print 'IK calculation step:DONE' 
+    
+    ## planning with joint target from IK 
+    planTraj =  arm.plan_jointTargetInput(jointTarg)
+    print 'Planning step with target joint angles:DONE' 
+      
+    ## planning with pose target
+    #print 'Planning step with target pose'   
+    #planTraj = arm.plan_poseTargetInput(tarPose)
+      
+    ## execution of the movement   
+    #print 'Execution of the plan' 
+    # arm.group[0].execute(planTraj)
+  
+if __name__ == '__main__':
+  ## First initialize moveit_commander and rospy.
+  moveit_commander.roscpp_initialize(sys.argv)
+  rospy.init_node('vector_basic_IK', anonymous=True)
+  
+  main()
+
---
+++ src/manipulation/manipulator.py
@@ -0,0 +1,436 @@
+import roslib; 
+from sensor_msgs.msg import JointState
+from vector_msgs.msg import JacoCartesianVelocityCmd, LinearActuatorCmd, GripperCmd, GripperStat
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from wpi_jaco_msgs.msg import AngularCommand, CartesianCommand
+#from wpi_jaco_msgs.srv import GravComp
+
+import rospy
+from math import pi, sqrt
+from collections import namedtuple
+from control_msgs.msg import FollowJointTrajectoryGoal, FollowJointTrajectoryAction
+import actionlib
+import time
+
+class Manipulator:
+  def __init__(self, arm_prefix = 'right'):
+    self.arm = Arm()
+    self.gripper = Gripper()
+    self.linear_actuator = LinearActuator()
+
+class Gripper:
+  def __init__(self):
+    self.pub_grp  = rospy.Publisher('/vector/right_gripper/cmd', GripperCmd, queue_size = 10)
+    self.cmd = GripperCmd()
+    
+    #i have it here but it is not useful
+    #rospy.Subscriber('/vector/right_gripper/joint_states', JointState, self.js_cb)
+    #self.last_js_update = None
+    #self.joint_state = None
+    
+    rospy.Subscriber('/vector/right_gripper/stat', GripperStat, self.st_cb)
+    self.last_st_update = None
+    self.gripper_stat = GripperStat()
+    
+
+  #def js_cb(self, inState):
+  #  self.joint_state = inState.position  
+  #  self.last_js_update = rospy.get_time()
+    
+  def st_cb(self, inStat):
+    self.gripperStat = inStat
+    self.last_st_update = None
+    
+  def is_ready(self):
+    return self.gripperStat.is_ready
+  
+  def is_reset(self):
+    return self.gripperStat.is_reset
+
+  def is_moving(self):
+    return self.gripperStat.is_moving
+    
+  def object_detected(self):
+    return self.gripperStat.obj_detected
+    
+  def get_pos(self):
+    return self.gripperStat.position
+    
+  def get_commanded_pos(self):
+    return self.gripperStat.requested_position
+
+  def get_applied_current(self):
+    return self.gripperStat.current
+
+  def set_pos(self, position, speed = 0.02, force = 100, rate = 10, iterations = 5):
+    
+    self.cmd.position = position
+    self.cmd.speed = speed
+    self.cmd.force = force
+    rrate = rospy.Rate(rate)
+    for i in range(0,iterations):
+      self.pub_grp.publish(self.cmd)
+      rrate.sleep()  
+    
+  def open(self, speed = 0.02, force = 100):
+    self.set_pos(0.085,speed,force)
+    
+  def close(self, speed = 0.02, force = 100):
+    self.set_pos(0,speed,force)
+
+class LinearActuator:
+  def __init__(self):
+    self.pub_lin  = rospy.Publisher('/vector/linear_actuator_cmd', LinearActuatorCmd, queue_size = 10)
+    self.cmd = LinearActuatorCmd()
+
+    #i agree that the naming is weird
+    rospy.Subscriber('/vector/joint_states', JointState, self.js_cb)
+    self.last_js_update = None
+    self.joint_state = None
+    
+  def js_cb(self, inState):
+    self.joint_state = inState.position  
+    self.last_js_update = rospy.get_time()
+
+
+  def set_pos(self, position, vel = 0.):
+    self.cmd = LinearActuatorCmd()
+    self.cmd.desired_position_m = position
+    if not vel == 0:
+      print 'What are you thinking? Setting the vel back to 0. If you are sure, change this line in the code'
+      vel = 0.
+
+    #probably feed forward velocity
+    self.cmd.fdfwd_vel_mps = vel
+
+    self.pub_lin.publish(self.cmd) 
+
+class Arm:
+  def __init__(self, arm_prefix = 'right'):
+    self.pub_jaco_ang  = rospy.Publisher('/jaco_arm/angular_cmd', AngularCommand, queue_size = 10)
+    self.pub_jaco_cart = rospy.Publisher('/jaco_arm/cartesian_cmd', CartesianCommand, queue_size = 10)
+    
+    self._arm_prefix = arm_prefix
+    self.arm_joint_names = [  self._arm_prefix + "_shoulder_pan_joint",   self._arm_prefix + "_shoulder_lift_joint",   self._arm_prefix + "_elbow_joint", 
+                              self._arm_prefix + "_wrist_1_joint",   self._arm_prefix + "_wrist_2_joint",   self._arm_prefix + "_wrist_3_joint"]
+
+    self.joint_states = [0 for i in range(0,len( self.arm_joint_names))]
+    
+    rospy.Subscriber('/vector/right_arm/joint_states', JointState, self.js_cb)
+    self.last_js_update = None
+    
+    self.smooth_joint_trajectory_client = actionlib.SimpleActionClient('/jaco_arm/joint_velocity_controller/trajectory', FollowJointTrajectoryAction)
+    
+    if(self.smooth_joint_trajectory_client.wait_for_server(rospy.Duration(5.0))):
+      self.traj_connection = True
+    else:
+      self.traj_connection = False
+
+    self.angular_cmd = AngularCommand()
+    self.angular_cmd.armCommand = True
+    self.angular_cmd.fingerCommand = False
+    self.angular_cmd.repeat = True
+
+    self.cartesian_cmd = CartesianCommand()
+    self.cartesian_cmd.armCommand = True
+    self.cartesian_cmd.fingerCommand = False
+    self.cartesian_cmd.repeat = True
+    
+    self._init_tuck_poses()
+
+#    if(rospy.wait_for_service('/jaco_arm/grav_comp')):
+#      self.gc_connection = True
+#    else:
+    self.gc_connection = False    
+#    self.grav_comp_client = rospy.ServiceProxy('/jaco_arm/grav_comp', GravComp)
+      
+  def enableGravComp(self):
+    #if(not self.gc_connection):
+    #  print 'GravComp Service not available'
+    print self.grav_comp_client(True)
+
+  def disableGravComp(self):
+    #if(not self.gc_connection):
+    #  print 'GravComp Service not available'
+    print self.grav_comp_client(False)
+      
+
+  def js_cb(self, inState):
+    for i in range(0,len(inState.position)):
+      self.joint_states[i] = inState.position[i]
+     
+    self.last_js_update = rospy.get_time()
+    
+  def get_pos(self):
+    return self.joint_states
+
+  def ang_pos_cmd(self, angles):
+    if not len(angles) == len(self.arm_joint_names):
+      print "Number of desired joint angles does not match the number of available joints"
+      return
+    self.angular_cmd.position = True
+    self.angular_cmd.joints = angles
+    self.pub_jaco_ang.publish(self.angular_cmd) 
+
+  def ang_vel_cmd(self, velocities):
+    if not len(velocities) == len(self.arm_joint_names):
+      print "Number of desired joint velocities does not match the number of available joints"
+      return
+    self.angular_cmd.position = False
+    self.angular_cmd.joints = velocities
+    self.pub_jaco_ang.publish(self.angular_cmd) 
+
+  def cart_pos_cmd(self, pose):
+    if not len(pose) == 6:
+      print "Not enough pose parameters specified"
+      return
+    self.cartesian_cmd.position = True
+    self.cartesian_cmd.arm.linear.x = pose[0]
+    self.cartesian_cmd.arm.linear.y = pose[1]
+    self.cartesian_cmd.arm.linear.z = pose[2]
+        
+    self.cartesian_cmd.arm.angular.x = pose[3]
+    self.cartesian_cmd.arm.angular.y = pose[4]
+    self.cartesian_cmd.arm.angular.z = pose[5]
+
+    self.pub_jaco_cart.publish(self.cartesian_cmd)
+
+  def cart_pos_cmd(self, translation, rotation):
+    if not len(translation) == 3:
+      print "Not enough translations specified"
+      return
+    if not len(rotation) == 3:
+      print "Not enough rotations specified"
+      return
+    pose = translation + rotation
+    self.cart_pos_cmd(pose)
+
+
+  def cart_vel_cmd(self, vels):
+    if not len(vels) == 6:
+      print "Not enough velocities specified"
+      return
+    self.cartesian_cmd.position = False
+    self.cartesian_cmd.arm.linear.x = vels[0]
+    self.cartesian_cmd.arm.linear.y = vels[1]
+    self.cartesian_cmd.arm.linear.z = vels[2]
+        
+    self.cartesian_cmd.arm.angular.x = vels[3]
+    self.cartesian_cmd.arm.angular.y = vels[4]
+    self.cartesian_cmd.arm.angular.z = vels[5]
+
+    self.pub_jaco_cart.publish(self.cartesian_cmd)
+
+  def cart_vel_cmd(self, translation, rotation):
+    if not len(translation) == 3:
+      print "Not enough translation velocities specified"
+      return
+    if not len(rotation) == 3:
+      print "Not enough rotation velocities specified"
+      return
+    vels = translation + rotation
+    self.cart_pos_cmd(vels)
+    
+  def ang_cmd_loop(self,angles,rate=10,iterations=5):
+    rrate = rospy.Rate(rate)
+    for i in range(0,iterations):
+      self.ang_pos_cmd(angles)
+      rrate.sleep()  
+      
+  def ang_cmd_wait(self,angles,epsilon=0.05, maxIter=50, rate=10):
+    error = epsilon + 1;
+    iterNum = 0;
+    #self.ang_cmd_loop(angles,rate)
+    self.ang_pos_cmd(angles)
+    rrate = rospy.Rate(rate)
+    while error > epsilon and iterNum < maxIter:
+      error = vectorDiff(self.joint_states,angles)
+      iterNum += 1
+      rrate.sleep()
+      
+    if iterNum == maxIter:
+      return False
+    return True
+    
+  #the full handling of vels, accs and effs will come later
+  # only the waypoints are needed for wpi jaco! the rest gets thrown away anyway so feel free to skip
+  def sendWaypointTrajectory(self, waypoints, durations = 0., vels = 0., accs = 0., effs = 0.):
+    print 'got trajectory:'
+    print waypoints
+    if not self.ang_cmd_wait(waypoints[0]):
+      print 'Cannot go to the first point in the trajectory'
+      return None
+#    else: 
+#      print 'Went to first'
+      
+    if not self.traj_connection:
+      print 'Action server connection was not established'
+      return None
+    joint_traj = JointTrajectory()
+    joint_traj.joint_names = self.arm_joint_names;
+    
+    if not durations == 0:
+      if not len(durations) == waypoints:
+        raise Exception('The number of duration points is not equal to the number of provided waypoints')
+    if not vels == 0:
+      if not len(vels) == waypoints:
+        raise Exception('The number velocity points is not equal to the number of provided waypoints')
+    if not accs == 0:
+      if not len(accs) == waypoints:
+        raise Exception('The number acceleration points is not equal to the number of provided waypoints')
+    if not effs == 0:
+      if not len(effs) == waypoints:
+        raise Exception('The number effort points is not equal to the number of provided waypoints')
+
+    if not effs == 0:    
+      if not (vels == 0 and accs == 0):
+        raise Exception('Cannot specify efforts with velocities and accelerations at the same time')
+    if (not accs == 0) and vels == 0:
+      raise Exception('Cannot specify accelerations without velocities')
+    
+    for t in range(0, len(waypoints)):
+      point = JointTrajectoryPoint()
+  
+      waypoint = waypoints[t]   
+      if not len(waypoint) == len(joint_traj.joint_names):
+        raise Exception('The number of provided joint positions is not equal to the number of available joints for index: ' + str(t))
+      point.positions = waypoint
+    
+      if not vels == 0.:
+        velocity = vels[t]
+        if not len(velocity) == len(joint_traj.joint_names):
+          raise Exception('The number of provided joint velocities is not equal to the number of available joints for index: ' + str(t))
+        point.velocities = velocity
+
+      if not accs == 0.:
+        acceleration = accs[t]
+        if not len(acceleration) == len(joint_traj.joint_names):
+          raise Exception('The number of provided joint accelerations is not equal to the number of available joints for index: ' + str(t))
+        point.accelerations = accelerations
+      
+      if not effs == 0.:
+        effort = effs[t]
+        if not len(effort) == len(joint_traj.joint_names):
+          raise Exception('The number of provided joint efforts is not equal to the number of available joints for index: ' + str(t))
+        point.effort = effort 
+      
+      if not durations == 0.:
+        point.duration = duration
+      
+      joint_traj.points.append(point)
+    
+    traj_goal = FollowJointTrajectoryGoal()
+    traj_goal.trajectory = joint_traj
+    #print traj_goal
+    #print self.joint_states
+
+    self.smooth_joint_trajectory_client.send_goal(traj_goal)
+   
+    self.smooth_joint_trajectory_client.wait_for_result()
+   
+    return self.smooth_joint_trajectory_client.get_result() 
+
+#TODO: figure this out
+  def upper_tuck(self, vanilla = False):
+    if vanilla:
+      self.sendWaypointTrajectory(self.ut_wps)
+    else:
+      self._ut_with_network()
+
+  def upper_untuck(self, vanilla = False):
+    if vanilla:
+      self.sendWaypointTrajectory(self.un_ut_wps)
+    else:
+      self.untuck()
+
+  def lower_tuck(self, vanilla = False):
+    if vanilla:
+      self.sendWaypointTrajectory(self.lt_wps)
+    else:
+      self._lt_with_network()
+
+  def lower_untuck(self, vanilla = False):
+    if vanilla:
+      self.sendWaypointTrajectory(self.un_lt_wps)
+    else:
+      self.untuck()
+    
+  def untuck(self):
+    self._untuck_with_network()
+
+  def _init_tuck_poses(self):
+    self.mid_wp = [-1.57, 3.14, 1.05, -1.57, 1.05, 1.57]
+    
+    lt_wp0 = [-1.65, 3.68, 1.12, -2.13, 1.48, 2.10]
+    lt_wp1 = [-1.49, 4.00, 1.47, -1.74, 1.25, 1.96]
+    lt_wp2 = [-1.23, 4.50, 0.95, -2.31, 1.82, 1.96]
+    lt_wp3 = [-1.21, 4.76, 0.83, -2.60, 2.56, 1.63]
+
+    self.lt_wps = [lt_wp0, lt_wp1, lt_wp2, lt_wp3]
+    self.un_lt_wps = self.lt_wps[::-1]
+
+    ut_wp0 = [-1.60, 2.20, 0.80, -2.20, 1.50, 1.20]
+    ut_wp1 = [-1.70, 2.00, 1.00, -2.20, 2.00, 0.90]
+    ut_wp2 = [-1.80, 1.80, 1.00, -2.10, 2.50, 0.72]
+    ut_wp3 = [-1.90, 1.50, 0.50, -2.00, 3.0, 0.72]
+
+    self.ut_wps = [ut_wp0,ut_wp1,ut_wp2,ut_wp3]
+    self.un_ut_wps = self.ut_wps[::-1]
+    
+    self.tuck_network          = self.un_lt_wps + [self.mid_wp] + self.ut_wps
+    self.reversed_tuck_network = self.tuck_network[::-1]
+    
+  def _find_closest_tuck_wp(self, tuck_wps, max_allowed_dist = 8.0):
+    if self.last_js_update is not None:
+      if self.last_js_update + 2.0 < rospy.get_time():
+        print 'The newest joint state information is too old.'
+        return None
+    
+    minDiff = 1000;    
+    minInd = -1
+    for i in range(0,len(tuck_wps)):
+      diff = vectorDiff(tuck_wps[i],self.joint_states)
+      if diff < minDiff:
+        minDiff = diff
+        minInd = i
+        
+    if minDiff > max_allowed_dist:
+      print 'Current arm configuration ' + str(self.joint_states) + ' is too far from the tuck network: ' + str(minDiff) + ' ' + str(minInd)
+      return None
+    
+    return minInd
+    
+  def _lt_with_network(self):
+    ind = self._find_closest_tuck_wp(self.reversed_tuck_network)
+    if ind is not None:
+      self.sendWaypointTrajectory(self.reversed_tuck_network[ind:])
+
+  def _ut_with_network(self):
+    ind = self._find_closest_tuck_wp(self.tuck_network)
+    if ind is not None:
+      self.sendWaypointTrajectory(self.tuck_network[ind:])
+    
+  def _untuck_with_network(self):
+    ind = self._find_closest_tuck_wp(self.tuck_network)
+    if ind is not None:
+      midPoint = 4;
+      if ind == midPoint:
+        wps = self.tuck_network[midPoint]
+      elif ind < midPoint:
+        wps = self.tuck_network[ind:midPoint+1]
+      else:    
+        wps = self.reversed_tuck_network[(len(self.reversed_tuck_network)-ind+1):midPoint+1]
+      
+      self.sendWaypointTrajectory(wps)
+      
+def vectorDiff(v1,v2):
+ error = 0;
+ l = min(len(v1),len(v2))
+ for i in range(0,l):
+   diff = (v1[i] - v2[i])
+   error += diff*diff;
+ error = sqrt(error)
+ return error
+    
+
+
